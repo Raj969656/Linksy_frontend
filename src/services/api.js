98c687a70bf1d31f2320552dfe594b2bd20eb8a7
Raj Yadav <mr.rajyadav272207@gmail.com>
2026-02-02 22:57:30 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,12 @@ const API = axios.create({
   baseURL: "https://url-shortner-backend-x55x.onrender.com",
 });
 
+// attach userId if logged in
+API.interceptors.request.use((config) => {
+  if (window.Clerk?.user) {
+    config.headers["x-user-id"] = window.Clerk.user.id;
+  }
+  return config;
+});
+
 export default API;
